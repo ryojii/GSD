2cfcfc3840a2ed80ebcae76748088d82dd9f54bb
jaig-sticky <jerome.aiguillon@stickyads.tv>
2017-04-12 16:47:44 +0200

    rename file

---
+++ v9/execution.go
@@ -0,0 +1,15 @@
+package main
+
+import "time"
+
+type Exec struct {
+	IdExec        int   `json:"idexec"`
+	Name      string    `json:"name"`
+	Status	  string    `json:"status"`
+	Trace	  string    `json:"trace"`
+	ForcedStatus string `json:"fstatus"`
+	StartDate time.Time `json:"start"`
+	EndDate   time.Time `json:"end"`
+}
+
+type Execs []Exec
